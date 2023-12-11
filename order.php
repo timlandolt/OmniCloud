50fd9fc8f401d5611467a66ee3d54d218ace649f
GitRizzard <150685306+GitRizzard@users.noreply.github.com>
2023-12-11 14:01:16 +0100

    Updated order.php
    + Variable $kunde hinzugefügt
    + func pushOrder angefangen
    ~ func testOrder geändert, Push
       rausgenommen

--- order.php
+++ order.php
@@ -5,27 +5,31 @@ $small_server = ["cpu" => 4, "ram" => 32, "ssd" => 4000];
 $medium_server = ["cpu" => 8, "ram" => 64, "ssd" => 8000];
 $big_server = ["cpu" => 16, "ram" => 128, "ssd" => 16000];
 
-function order() {
+$kunde = "";
 
+function testOrder($cores, $ram, $storage) {
+
+    global $kunde;
     global $small_server;
     global $medium_server;
     global $big_server;
-    if ($_POST["cpu"] < $small_server["cpu"] && $_POST["ram"] < $small_server["ram"] && $_POST["ssd"] < $small_server["ssd"]) {
-        $small_server["cpu"] -= $_POST["cpu"];
-        $small_server["ram"] -= $_POST["ram"];
-        $small_server["ssd"] -= $_POST["ssd"];
-    } elseif ($_POST["cpu"] < $medium_server["cpu"] && $_POST["ram"] < $medium_server["ram"] && $_POST["ssd"] < $medium_server["ssd"]) {
-        $medium_server["cpu"] -= $_POST["cpu"];
-        $medium_server["ram"] -= $_POST["ram"];
-        $medium_server["ssd"] -= $_POST["ssd"];
-    } elseif ($_POST["cpu"] < $big_server["cpu"] && $_POST["ram"] < $big_server["ram"] && $_POST["ssd"] < $big_server["ssd"]) {
-        $big_server["cpu"] -= $_POST["cpu"];
-        $big_server["ram"] -= $_POST["ram"];
-        $big_server["ssd"] -= $_POST["ssd"];
+    if ($cores < $small_server["cpu"] && $ram < $small_server["ram"] && $storage < $small_server["ssd"]) {
+        pushOrder($kunde, "small", $cores, $ram, $storage);
+    } elseif ($cores < $medium_server["cpu"] && $ram < $medium_server["ram"] && $storage < $medium_server["ssd"]) {
+        pushOrder($kunde, "medium", $cores, $ram, $storage);
+    } elseif ($cores < $big_server["cpu"] && $ram < $big_server["ram"] && $storage < $big_server["ssd"]) {
+        pushOrder($kunde, "big", $cores, $ram, $storage);
     } else {
         //popup: "Zu grosse Angaben!"
     }
 
+}
+
+function pushOrder($kunde, $server, $cores, $ram, $storage){
+
+    $myfile = fopen("kunden.txt", "a") or die("Unable to open file!");
+    $input = $kunde." ".$server." ".$cores." ".$ram." ".$storage;
+    fwrite($myfile, $input);
 }
 
 ?>
