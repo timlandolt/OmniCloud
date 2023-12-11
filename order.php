1c01e6b46d69cc72238f9fa7327764b24d779470
GitRizzard <150685306+GitRizzard@users.noreply.github.com>
2023-12-11 17:12:59 +0100

    Backend logic finished ~ funct testOrder funktion     eingebaut ~ funct getServerData funktion um aktuelle Serverspecs zu   bekommen ~ funct pushOrder auf          restlichen Code angepasst

--- order.php
+++ order.php
@@ -1,5 +1,5 @@
 <?php
-$virtual_machines = [];
+$vmCount = 0;
 
 $small_server = ["cpu" => 4, "ram" => 32, "ssd" => 4000];
 $medium_server = ["cpu" => 8, "ram" => 64, "ssd" => 8000];
@@ -7,48 +7,83 @@ $big_server = ["cpu" => 16, "ram" => 128, "ssd" => 16000];
 
 $kunde = "";
 
-function getServerData(){
-    $myfile = fopen("kunden.txt", "r");
+getServerData();
 
-    while(! feof($myfile))
-    {
-        foreach (fgets($myfile) as $line){
-            $dataElements = explode(",", $line);
-            if ($dataElements[1] == "small") {
+if (isset($_POST[""]))                                                  //Tim
+testOrder($_POST[""], $_POST[""], $_POST[""]);                          //Tim
+function getServerData()
+{
 
-            } elseif ($dataElements[1] == "medium") {
-
-            } elseif ($dataElements[1] == "big") {
-
-            }
-        };
-    }
-
-    fclose($myfile);
-}
-
-function testOrder($cores, $ram, $storage) {
-
-    global $kunde;
+    global $vmCount;
     global $small_server;
     global $medium_server;
     global $big_server;
-    if ($cores < $small_server["cpu"] && $ram < $small_server["ram"] && $storage < $small_server["ssd"]) {
-        pushOrder($kunde, "small", $cores, $ram, $storage);
-    } elseif ($cores < $medium_server["cpu"] && $ram < $medium_server["ram"] && $storage < $medium_server["ssd"]) {
-        pushOrder($kunde, "medium", $cores, $ram, $storage);
-    } elseif ($cores < $big_server["cpu"] && $ram < $big_server["ram"] && $storage < $big_server["ssd"]) {
-        pushOrder($kunde, "big", $cores, $ram, $storage);
-    } else {
-        //popup: "Zu grosse Angaben!"
-    }
 
+    $myfile = file("kunden.txt");
+
+    foreach ($myfile as $line) {
+
+        $vmCount++;
+
+        $dataElements = explode(",", $line);
+
+        if ($dataElements[1] == "small") {
+
+            $small_server["cpu"] -= $dataElements[2];
+            $small_server["ram"] -= $dataElements[3];
+            $small_server["ssd"] -= $dataElements[4];
+
+        } elseif ($dataElements[1] == "medium") {
+
+            $medium_server["cpu"] -= $dataElements[2];
+            $medium_server["ram"] -= $dataElements[3];
+            $medium_server["ssd"] -= $dataElements[4];
+
+        } elseif ($dataElements[1] == "big") {
+
+            $big_server["cpu"] -= $dataElements[2];
+            $big_server["ram"] -= $dataElements[3];
+            $big_server["ssd"] -= $dataElements[4];
+
+        }
+    };
 }
 
-function pushOrder($kunde, $server, $cores, $ram, $storage){
+function testOrder($cores, $ram, $storage)
+{
 
+    global $vmCount;
+    global $small_server;
+    global $medium_server;
+    global $big_server;
+    $server = "";
+
+    if ($cores <= $small_server["cpu"] && $ram <= $small_server["ram"] && $storage <= $small_server["ssd"]) {
+        $server = "small";
+    }
+
+    elseif ($cores <= $medium_server["cpu"] && $ram <= $medium_server["ram"] && $storage <= $medium_server["ssd"]) {
+        $server = "medium";
+    }
+
+    elseif ($cores <= $big_server["cpu"] && $ram < $big_server["ram"] && $storage <= $big_server["ssd"]) {
+        $server = "big";
+    }
+
+    else {
+                                                                //Tim
+    }
+
+    if (server != "") {
+        pushOrder($vmCount, $server, $cores, $ram, $storage);
+    }
+}
+
+function pushOrder($vmCount, $server, $cores, $ram, $storage)
+{
+    $vmCount += 1;
     $myfile = fopen("kunden.txt", "a");
-    $input = $kunde.",".$server.",".$cores.",".$ram.",".$storage;
+    $input = "\n".$vmCount . "," . $server . "," . $cores . "," . $ram . "," . $storage;
     fwrite($myfile, $input);
 }
 
@@ -63,6 +98,8 @@ function pushOrder($kunde, $server, $cores, $ram, $storage){
     <title>OmniCloud-Serverbestellen</title>
 </head>
 <body>
-
+<div id="wrapper">
+    <?php include('header.html'); ?>
+</div>
 </body>
 </html>
