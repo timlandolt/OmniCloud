211c30fbf60e6850e1120c2789f94f575c15092e
Tim Landolt <timlandolt@gmx.ch>
2023-12-12 12:51:23 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/main'""
    This reverts commit b6be6ad7e4ff39aa794887d9acaa6c0edce8518a.

--- order.php
+++ order.php
@@ -1,5 +1,5 @@
 <?php
-$virtual_machines = [];
+$vmCount = 0;
 
 $small_server = ["cpu" => 4, "ram" => 32, "ssd" => 4000];
 $medium_server = ["cpu" => 8, "ram" => 64, "ssd" => 8000];
@@ -7,50 +7,83 @@ $big_server = ["cpu" => 16, "ram" => 128, "ssd" => 16000];
 
 $kunde = "";
 
+getServerData();
+
+if (isset($_POST[""]))                                                  //Tim
+testOrder($_POST[""], $_POST[""], $_POST[""]);                          //Tim
 function getServerData()
 {
-    $myfile = fopen("kunden.txt", "r");
 
-    while (!feof($myfile)) {
-        foreach (fgets($myfile) as $line) {
-            $dataElements = explode(",", $line);
-            if ($dataElements[1] == "small") {
+    global $vmCount;
+    global $small_server;
+    global $medium_server;
+    global $big_server;
 
-            } elseif ($dataElements[1] == "medium") {
+    $myfile = file("kunden.txt");
 
-            } elseif ($dataElements[1] == "big") {
+    foreach ($myfile as $line) {
 
-            }
-        };
-    }
+        $vmCount++;
 
-    fclose($myfile);
+        $dataElements = explode(",", $line);
+
+        if ($dataElements[1] == "small") {
+
+            $small_server["cpu"] -= $dataElements[2];
+            $small_server["ram"] -= $dataElements[3];
+            $small_server["ssd"] -= $dataElements[4];
+
+        } elseif ($dataElements[1] == "medium") {
+
+            $medium_server["cpu"] -= $dataElements[2];
+            $medium_server["ram"] -= $dataElements[3];
+            $medium_server["ssd"] -= $dataElements[4];
+
+        } elseif ($dataElements[1] == "big") {
+
+            $big_server["cpu"] -= $dataElements[2];
+            $big_server["ram"] -= $dataElements[3];
+            $big_server["ssd"] -= $dataElements[4];
+
+        }
+    };
 }
 
 function testOrder($cores, $ram, $storage)
 {
 
-    global $kunde;
+    global $vmCount;
     global $small_server;
     global $medium_server;
     global $big_server;
-    if ($cores < $small_server["cpu"] && $ram < $small_server["ram"] && $storage < $small_server["ssd"]) {
-        pushOrder($kunde, "small", $cores, $ram, $storage);
-    } elseif ($cores < $medium_server["cpu"] && $ram < $medium_server["ram"] && $storage < $medium_server["ssd"]) {
-        pushOrder($kunde, "medium", $cores, $ram, $storage);
-    } elseif ($cores < $big_server["cpu"] && $ram < $big_server["ram"] && $storage < $big_server["ssd"]) {
-        pushOrder($kunde, "big", $cores, $ram, $storage);
-    } else {
-        //popup: "Zu grosse Angaben!"
+    $server = "";
+
+    if ($cores <= $small_server["cpu"] && $ram <= $small_server["ram"] && $storage <= $small_server["ssd"]) {
+        $server = "small";
     }
 
+    elseif ($cores <= $medium_server["cpu"] && $ram <= $medium_server["ram"] && $storage <= $medium_server["ssd"]) {
+        $server = "medium";
+    }
+
+    elseif ($cores <= $big_server["cpu"] && $ram < $big_server["ram"] && $storage <= $big_server["ssd"]) {
+        $server = "big";
+    }
+
+    else {
+                                                                //Tim
+    }
+
+    if (server != "") {
+        pushOrder($vmCount, $server, $cores, $ram, $storage);
+    }
 }
 
-function pushOrder($kunde, $server, $cores, $ram, $storage)
+function pushOrder($vmCount, $server, $cores, $ram, $storage)
 {
-
+    $vmCount += 1;
     $myfile = fopen("kunden.txt", "a");
-    $input = $kunde . "," . $server . "," . $cores . "," . $ram . "," . $storage;
+    $input = "\n".$vmCount . "," . $server . "," . $cores . "," . $ram . "," . $storage;
     fwrite($myfile, $input);
 }
 
@@ -62,13 +95,11 @@ function pushOrder($kunde, $server, $cores, $ram, $storage)
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>OmniCloud | Server Bestellen</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>OmniCloud-Serverbestellen</title>
 </head>
 <body>
 <div id="wrapper">
-
-    <?php include('header.html') ?>
+    <?php include('header.html'); ?>
 </div>
 </body>
 </html>
